be3b0f63bfe3661a0e3b8c5647c3126a5a621811
Michał Zdziech <m.zdziech82@gmail.com>
2019-06-11 00:42:06 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Siema');
+const name = 'Michał';
+const age = 37;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('Witaj przybyszu ! :)');
