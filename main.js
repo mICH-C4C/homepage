f7f80cd726509b4d66d7650cc878ae62ca748a5b
Michał Zdziech <m.zdziech82@gmail.com>
2019-07-03 21:26:47 +0200

    Upiękrzanie strony, nadanie klas w selektorach i nadanie im właściwości za pomocą css'a

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ const age = 36;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się '${name}' i mam ${age} lat`);
-console.log('Witaj przybyszu ! :)' );
+console.log('Witaj przybyszu ! :) :rocket:');
